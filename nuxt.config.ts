2ed8345314256255e22fa18ae8af694837ed5316
thezzisu <thezzisu@gmail.com>
2021-05-17 20:35:03 +0800

    fix: font swap on navigate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,16 +29,8 @@ const config: NuxtConfig = {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-      {
-        rel: 'stylesheet',
-        media: 'print',
-        crossorigin: 'anonymous',
-        onload: `this.media='all'`,
-        href:
-          'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Roboto+Mono&family=Noto+Sans+SC:wght@200;300;400;500;700;900&display=swap'
-      },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      //
     ]
   },
   css: [
